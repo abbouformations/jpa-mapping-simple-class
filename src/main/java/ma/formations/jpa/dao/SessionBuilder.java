f741c0894d3fe919aba569250a88960c32ca4f17
abbou <abbouformations@gmail.com>
2025-01-18 10:50:49 +0100

    change the provider

--- src/main/java/ma/formations/jpa/dao/SessionBuilder.java
+++ src/main/java/ma/formations/jpa/dao/SessionBuilder.java
@@ -1,10 +1,15 @@
 package ma.formations.jpa.dao;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class SessionBuilder {
 	private static EntityManagerFactory sessionFactory;
+	private static final Logger logger = LogManager.getLogger(SessionBuilder.class);
 	private SessionBuilder() {
 		
 	}
@@ -14,6 +19,7 @@ public class SessionBuilder {
 				sessionFactory=Persistence.createEntityManagerFactory("unite1");
 		} catch (Exception e) {
 			e.printStackTrace();
+			logger.error("{}", e);
 		}
 		return sessionFactory;
 	}
--- src/main/java/ma/formations/jpa/service/ServiceImpl.java
+++ src/main/java/ma/formations/jpa/service/ServiceImpl.java
@@ -9,9 +9,12 @@ import javax.persistence.Query;
 
 import ma.formations.jpa.dao.SessionBuilder;
 import ma.formations.jpa.service.model.Product;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SuppressWarnings({ "unchecked", "finally" })
 public class ServiceImpl implements IService {
+	private static final Logger logger = LogManager.getLogger(ServiceImpl.class);
 
 	@Override
 	public void saveProduct(Product p) {
@@ -23,11 +26,14 @@ public class ServiceImpl implements IService {
 			tx.begin();
 			session.merge(p);
 			tx.commit();
+			logger.info("saveProduct ok",p);
 		} catch (Exception ex) {
-			tx.rollback();
+			if (tx != null) tx.rollback();
 			ex.printStackTrace();
+			logger.error("{}", ex);
+
 		} finally {
-			session.close();
+			if (session != null) session.close();
 		}
 	}
 
@@ -39,8 +45,10 @@ public class ServiceImpl implements IService {
 		try {
 			session = SessionBuilder.getSessionFactory().createEntityManager();
 			result = session.createQuery("select a from Product a").getResultList();
+			logger.info("getAllProducts ok");
 		} catch (Exception ex) {
 			ex.printStackTrace();
+			logger.error("{}", ex);
 		} finally {
 			session.close();
 			return result;
@@ -60,9 +68,11 @@ public class ServiceImpl implements IService {
 			if (p != null)
 				session.remove(p);
 			tx.commit();
+			logger.info("removeProduct ok",p);
 		} catch (Exception ex) {
 			tx.rollback();
 			ex.printStackTrace();
+			logger.error("{}", ex);
 		} finally {
 			session.close();
 		} 
@@ -76,8 +86,10 @@ public class ServiceImpl implements IService {
 		try {
 			session = SessionBuilder.getSessionFactory().createEntityManager();
 			result = session.find(Product.class,id);
+			logger.info("getProdctById ok",id);
 		} catch (Exception ex) {
 			ex.printStackTrace();
+			logger.error("{}", ex);
 		} finally {
 			session.close();
 			return result;
@@ -94,8 +106,10 @@ public class ServiceImpl implements IService {
 			Query requete=session.createQuery("select p from Product p where p.designation like :d");
 			requete.setParameter("d","%"+designation+"%");
 			result=requete.getResultList();
+			logger.info("getProductsByDesignation ok",designation);
 		} catch (Exception ex) {
 			ex.printStackTrace();
+			logger.error("{}", ex);
 		} finally {
 			session.close();
 			return result;
